e5c39485614c786dc41ba5a875ad103d7e06f072
Robinson Lopez <robins.616009@gmail.com>
2025-04-22 17:13:52 -0500

    Modelo, Clase Rol

--- src/main/java/com/sistema/examenes/modelo/Rol.java
+++ src/main/java/com/sistema/examenes/modelo/Rol.java
@@ -1,16 +1,18 @@
 package com.sistema.examenes.modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
 public class Rol {
 
     @Id
-    private Long id;
-
-    private String rolId;
+    private Long rolId;
     private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "usuario")
+    private Set<UsuarioRol> usuarioRoles = new HashSet<>();
 }
