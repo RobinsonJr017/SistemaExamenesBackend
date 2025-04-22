86f65cc1400641b3af48390291858e6aedc1dd95
Robinson Lopez <robins.616009@gmail.com>
2025-04-22 16:55:01 -0500

    Modelo, Clase identidad Usuario

---
+++ src/main/java/com/sistema/examenes/modelo/Usuario.java
@@ -0,0 +1,23 @@
+package com.sistema.examenes.modelo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private boolean enabled = true;
+    private String perfil;
+    
+
+}
