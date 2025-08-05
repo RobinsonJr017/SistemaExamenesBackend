4db7d0b823a21e7a18b37eabc942ee89a8747fff
Robinson Lopez <robins.616009@gmail.com>
2025-08-04 20:01:03 -0500

    Correcion controller, añadir usuario (rol)

--- src/main/java/com/sistema/examenes/controladores/UsuarioController.java
+++ src/main/java/com/sistema/examenes/controladores/UsuarioController.java
@@ -12,6 +12,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin("*")
 public class UsuarioController {
 
     @Autowired
@@ -19,7 +20,8 @@ public class UsuarioController {
 
     @PostMapping("/")
     public Usuario guardarUsuario(@RequestBody Usuario usuario) throws  Exception{
-        Set<UsuarioRol> roles = new HashSet<>();
+        usuario.setPerfil("default.png");
+        Set<UsuarioRol> usuarioRoles = new HashSet<>();
 
         Rol rol = new Rol();
         rol.setRolId(2L);
@@ -29,7 +31,8 @@ public class UsuarioController {
         usuarioRol.setUsuario(usuario);
         usuarioRol.setRol(rol);
 
-        return usuarioService.guardarUsuario(usuario, roles);
+        usuarioRoles.add(usuarioRol);
+        return usuarioService.guardarUsuario(usuario, usuarioRoles);
     }
 
     @GetMapping("/{username}")
